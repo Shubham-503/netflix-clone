9812899127f0f85c29fab3308de64764b5d561bf
Shubham <kr.shubham025@gmail.com>
2020-10-19 14:16:40 +0530

    Row setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    </div>
+  );
 }
 
 export default App;
